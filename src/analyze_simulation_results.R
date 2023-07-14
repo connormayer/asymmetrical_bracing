16b24019c2f8113e2f0bccbfedc1270f74c4a2a1
Connor Mayer <cjmayer@uci.edu>
2023-07-14 12:55:24 -0600

    Tidied up repo

--- src/analyze_simulation_results.R
+++ src/analyze_simulation_results.R
@@ -1,10 +1,8 @@
 library(tidyverse)
 library(lme4)
-library(nnet)
 
 # Change this to your local directory
-setwd("C:/Users/conno/git_repos/asymmetrical_bracing/")
-#setwd("E:/git_repos/asymmetrical_bracing/")
+setwd("E:/git_repos/asymmetrical_bracing/")
 
 # You may need to change these
 data_folder <- "data/50-150_jaw_simulations"
@@ -18,6 +16,8 @@ contacts_headers <- c(
   "sim", "front", "mid", "back", "lat_left", "lat_right", "coronal"
 )
 
+# Get the output of the Artisynth simulations and read contacts,
+# excitations, and failures into separate tibbles
 for (f in list.files(data_folder)) {
   full_path <- file.path(data_folder, f)
   data <- read_csv(full_path, col_names=FALSE,col_types = cols())
@@ -32,19 +32,22 @@ for (f in list.files(data_folder)) {
 
 colnames(contacts_df) <- contacts_headers
 
+# Read csv that relates simulation numbers to specific levels of
+# agonist/antagonist activation
 groupings_df <- read_csv(groupings_file)
 colnames(groupings_df)[1] <- 'sim'
 
+# Join contact results to groupings CSV based on simulation number.
 success_df <- inner_join(contacts_df, groupings_df, by = c("sim"))
 success_df$condition <- paste("Agonists:", success_df$L_AG, "\nAntagonists:", success_df$L_ANT)
-success_df <- success_df %>% filter(SL_ACT == 1)
 
+# Mean contacts in different conditions
 mean_contacts <- success_df %>%
   select(condition, front, mid, back, lat_left, lat_right, coronal) %>%
   group_by(condition) %>%
   summarise(across(c(front, mid, back, lat_left, lat_right, coronal), mean, na.rm=TRUE))
-mean_contacts
 
+# Mean activations for each condition
 mean_activations <- success_df %>%
   select(condition, IL_L, IL_R, GGM_L, GGM_R, GGP_L, GGP_R, VERT_L, VERT_R,
          HG_L, HG_R, STY_L, STY_R, MH_L, MH_R, SL, TRANS_L, TRANS_R,
@@ -53,21 +56,17 @@ mean_activations <- success_df %>%
   summarise(across(c(IL_L, IL_R, GGM_L, GGM_R, GGP_L, GGP_R, VERT_L, VERT_R,
                      HG_L, HG_R, STY_L, STY_R, MH_L, MH_R, SL, TRANS_L, TRANS_R,
                      GGA_L, GGA_R), mean, na.rm=TRUE))
-mean_activations
-
 
 print("Successful sims")
 print(success_df %>% group_by(condition) %>% count())
 
 print("Mean bracing activations")
 print(mean_activations)
-# 
-# print("Mean unilateral bracing activations")
-# print(mean_uni_activations)
-# 
+
 print("Mean contacts")
 print(mean_contacts)
 
+# Make a data frame for plotting purposes
 plot_df <- success_df %>%
   mutate(left = ifelse(lat_left > 0, 1, 0),
          right = ifelse(lat_right > 0, 1, 0)) %>%
@@ -75,17 +74,19 @@ plot_df <- success_df %>%
   pivot_longer(cols=c("left", "right"), 
                names_to='side', values_to='contacts')
 
+# Barplot of L/R contact by condition
 ggplot(plot_df) +
   geom_bar(aes(x=condition, y=contacts, fill=side), 
            stat='summary', fun='mean', position='dodge') +
   ylab("Proportion of simulations with bracing") +
   xlab("Condition") +
   scale_fill_discrete(name = "Bracing side") +
-  theme_classic(base_size=20)
-# 
-# print("Mean unilateral contacts")
-# print(mean_uni_contacts)
-# 
+  theme_classic(base_size=22) +
+  theme(legend.text=element_text(size=23),
+        legend.title=element_text(size=23),
+        axis.text.x = element_text(size=20),
+        legend.position = c(0.2, 0.9))
+
 # Compare activation by bracer/non-bracer category
 cat_data <- success_df %>%
   select(L_AG, L_ANT, SL_ACT, IL_L, IL_R, GGM_L, GGM_R, GGP_L, GGP_R, VERT_L, VERT_R,
@@ -103,18 +104,21 @@ cat_pivot %>% group_by(L_AG, L_ANT, SL_ACT, type) %>% summarize(mean=mean(activa
 # Stats
 activation_df_stats <- success_df %>%
   mutate(
-    contact = ifelse(front > 0 | mid > 0 | back > 0 | lat_left > 0 | lat_right > 0 | coronal > 0, 'contact', 'no contact'),
+    contact = ifelse(front > 0 | mid > 0 | back > 0 | lat_left > 0 | lat_right > 0 | coronal > 0, 
+                     'contact', 'no contact'),
     r_bracing = ifelse(lat_right > 0, TRUE, FALSE),
     l_bracing = ifelse(lat_left > 0, TRUE, FALSE),
     L_ANT = scale(L_ANT),
     L_AG = scale(L_AG)
   ) 
 
-# Coarse analysis
+# Coarse analysis. This is what's used in the paper.
+# Model predicting right-side bracing
 r_m_coarse <- glm(r_bracing ~ L_AG * L_ANT,
            data = activation_df_stats, family = 'binomial')
 summary(r_m_coarse)
 
+# Model predicting left-side bracing
 l_m_coarse <- glm(l_bracing ~ L_AG * L_ANT,
                   data = activation_df_stats, family = 'binomial')
 summary(l_m_coarse)
@@ -125,7 +129,7 @@ l_m <- glm(l_bracing ~ GGP_L + GGM_L + GGA_L + STY_L + MH_L + HG_L + TRANS_L + V
            data = activation_df_stats, family = 'binomial')
 summary(l_m)
 
-# Fine analysis
+# Fine analysis by individual muscles. These results aren't used in the paper.
 r_m <- glm(r_bracing ~ GGP_L + GGM_L + GGA_L + STY_L + MH_L + HG_L + TRANS_L + VERT_L + IL_L,
          data = activation_df_stats, family = 'binomial')
 summary(r_m)
@@ -133,43 +137,3 @@ summary(r_m)
 l_m <- glm(l_bracing ~ GGP_L + GGM_L + GGA_L + STY_L + MH_L + HG_L + TRANS_L + VERT_L + IL_L,
            data = activation_df_stats, family = 'binomial')
 summary(l_m)
-
-# # Visualization
-# 
-# # Histogram of activation level counts
-# 
-# activation_df <- success_df %>%
-#   mutate(
-#     contact = ifelse(front > 0 | mid > 0 | back > 0 | lat_left > 0 | lat_right > 0 | coronal > 0, 'contact', 'no contact'),
-#     bracing = ifelse(lat_left > 0 & lat_right > 0, 'bi_bracing', ifelse(lat_right | lat_left, 'uni_bracing', 'no bracing')),
-#     group = str_c(contact, bracing, sep = ' & ')
-#   ) %>%
-#   select(condition, contact, bracing, group, IL_L, IL_R, GGM_L, GGM_R, GGP_L, GGP_R, VERT_L, VERT_R,
-#          HG_L, HG_R, STY_L, STY_R, MH_L, MH_R, SL, TRANS_L, TRANS_R,
-#          GGA_L, GGA_R)
-# 
-# activation_df %>%
-#   mutate(bracing = factor(bracing, levels = c('bi_bracing', 'no bracing', 'uni_bracing'))) %>%
-#   gather(key, value, -condition, -contact, -bracing, -group) %>%
-#   ggplot(aes(x=value, fill=bracing)) +
-#   geom_bar(aes(y=..prop..), position=position_dodge(preserve = "single")) +
-#   facet_wrap(~key, nrow=2) +
-#   xlab("Activation level") +
-#   ylab("Proportion of bracing outcomes")
-# ggsave("images/activation_histogram.png")
-# 
-# # Barplot of mean levels
-# 
-# pivot_data %>%
-#   mutate(condition = factor(condition, levels = c("5mm", "10mm"))) %>%
-#   ggplot(aes(x=factor(muscle, 
-#                       levels = c("HG", "TRANS", "STY", "GGA", "IL", "VERT", "GGM", "MH", "SL", "GGP")), 
-#              y=mean, fill=condition)) +
-#   geom_bar(position=position_dodge(preserve = "single"), stat='identity') +
-#   xlab("Muscle") +
-#   ylab("Mean activation level for bilateral bracing outcomes") + 
-#   scale_fill_discrete(name = "Jaw Opening") +
-#   facet_grid(~ type, scale='free') + 
-#   theme(text = element_text(size=20))
-# ggsave("images/activation_barplot.png")
-
--- src/generate_batchsim.py
+++ src/generate_batchsim.py
@@ -1,6 +1,9 @@
 import csv
 import pandas
 
+# This script generates the simulations used in this paper
+
+# Designate agonists and antagonists
 AGONISTS = [
     "VERT",
     "GGM",
@@ -16,44 +19,56 @@ ANTAGONISTS = [
     "IL"
 ]
 
+# Load the file of original bracing outcomes from Liu et al. (2022)
 df = pandas.read_csv("data/bracing_activations.csv")
 
-# Just keep muscle activations, plus new columns reflecting conditions
-new_header = []
-
+# Just keep muscle activations
 muscle_names = list(set(df.columns[8:18]))
 
+# Build each of the four simulations starting from original one
 new_rows = []
 for index, row in df.iterrows():
+    # Activate antagonists at 100% or 150%
     for l_ant_act in [1, 1.5]:
+        # Activate agonists at 50% or 100%
         for l_ag_act in [0.5, 1]:
-                    new_row = {}
-                    new_row['L_ANT'] = l_ant_act
-                    new_row['R_ANT'] = 1
-                    new_row['L_AG'] = l_ag_act
-                    new_row['R_AG'] = 1
-                    new_row['SL_ACT'] = 1
+            # Record details of activations
+            new_row = {}
+            new_row['L_ANT'] = l_ant_act
+            new_row['R_ANT'] = 1
+            new_row['L_AG'] = l_ag_act
+            new_row['R_AG'] = 1
+            new_row['SL_ACT'] = 1
 
-                    for muscle in muscle_names:
-                        if muscle == "SL":
-                            new_row["SL"] = row[muscle]
-                            continue
+            # Scale original muscle activation by relevant factor
+            for muscle in muscle_names:
+                # SL isn't lateralized, so keep as is
+                if muscle == "SL":
+                    new_row["SL"] = row[muscle]
+                    continue
 
-                        if muscle in ANTAGONISTS:
-                            l_mult = l_ant_act
-                        else:
-                            l_mult = l_ag_act
+                if muscle in ANTAGONISTS:
+                    l_mult = l_ant_act
+                else:
+                    l_mult = l_ag_act
 
-                        new_row[muscle + "_L"] = round(row[muscle] * l_mult, 3)
-                        new_row[muscle + "_R"] = row[muscle]
-                    new_row['BI_CLOSE'] = 0.0015
+                # Add columns for lateralized msucle activations
+                new_row[muscle + "_L"] = round(row[muscle] * l_mult, 3)
+                new_row[muscle + "_R"] = row[muscle]
+            # Jaw activation to get us to 5mm
+            new_row['BI_CLOSE'] = 0.0015
 
-                    new_rows.append(new_row)
+            new_rows.append(new_row)
 
+# Save a .csv that records the simulation number and its corresponding
+# muscle activations
 new_df = pandas.DataFrame(new_rows)
 new_df.to_csv("data/asymmetrical_activations_50_150_jaw.csv")
 
-# Create batchsim file
+# Create batchsim file to drive simulations in Artisynth. 
+# We use a combination of the simulation number and the 'redef'
+# command in BatchSim to run an arbitrary set of simulations,
+# rather than one based on grid search or sampling
 batchsim_rows = []
 batchsim_rows.append('@PHONY "simnum" = {{%{}%}}'.format(
     '% %'.join([str(x) for x in range(len(new_df))])
@@ -62,35 +77,23 @@ batchsim_rows.append('@PHONY "simnum" = {{%{}%}}'.format(
 muscle_names = new_df.iloc[:, 5:-1]
 
 for muscle in muscle_names:
-    batchsim_rows.append('"models/jawmodel/models/tongue/bundles/{}:excitation" = {{}}'.format(muscle))
+    batchsim_rows.append(
+        '"models/jawmodel/models/tongue/bundles/{}:excitation" = {{}}'.format(muscle)
+    )
 batchsim_rows.append('"models/jawmodel/exciters/bi_close:excitation" = {%0.0015%}')
 
 for idx, row in new_df.iterrows():
     batchsim_rows.append('redef')
     for muscle in muscle_names:
-        batchsim_rows.append('\t"models/jawmodel/models/tongue/bundles/{}:excitation" = {{%{}%}}'.format(muscle, row[muscle]))
+        batchsim_rows.append(
+            '\t"models/jawmodel/models/tongue/bundles/{}:excitation" = {{%{}%}}'.format(
+                muscle, row[muscle]
+            )
+        )
     batchsim_rows.append('when')
     batchsim_rows.append('\t"simnum" = {{%{}%}}'.format(str(idx)))
     batchsim_rows.append("end")
-#     breakpoint()
 
-# muscle_names = new_df.iloc[:, 5:]
-# muscle_activation_sets = {}
-
-# for muscle in muscle_names:
-#     muscle_activation_sets[muscle] = sorted(pandas.unique(new_df[muscle]))
-#     batchsim_rows.append('"{}"= {{%{}%}}'.format(
-#         muscle, "% %".join(map(str, muscle_activation_sets[muscle]))
-#     ))
-
-# muscles = []
-# activations = []
-
-# for muscle, activation in muscle_activation_sets.items():
-#     muscles.append(muscle)
-#     activations.append(activation)
-
-# muscles, activations = muscle_activation_sets.items()
-
+# Write the batchsim props file
 with open('data/props_50_150_jaw.psl', 'w') as f:
     f.write('\n'.join(batchsim_rows))
